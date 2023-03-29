c337e64d21899818ad62a605202bd8c03f4adbc3
meanjoo <meanjooo@naver.com>
2023-03-29 20:27:10 +0900

    edit draw_230320.js

--- draw_230320.js
+++ draw_230320.js
@@ -12,9 +12,6 @@ linePts.push(new THREE.Vector2(150, 200));
 linePts.push(new THREE.Vector2(50, 250));
 linePts.push(new THREE.Vector2(350, 250));
 
-linePts.push(new THREE.Vector2(50, 50));
-linePts.push(new THREE.Vector2(150, 200));
-
 let boxPts = [];
 
 boxPts.push(new THREE.Vector2(100, 100));
@@ -63,6 +60,8 @@ function draw_image() {
     ctx.strokeStyle = "black";
     draw_circle(new THREE.Vector2(230 + xValue, 230 + yValue), 110);
 
+    line_line_intersection(linePts[0], linePts[1], linePts[2], linePts[3])
+
     line_box_intersection(linePts[0], linePts[1], boxPts[0], boxPts[1])
     line_box_intersection(linePts[2], linePts[3], boxPts[0], boxPts[1])
 
@@ -87,13 +86,13 @@ function line_line_intersection(p0, p1, p2, p3) {
     let intersectionX = (b1 - b0) / (a0 - a1);
     let intersectionY = a0 * intersectionX + b0;
 
-    if (p0.x > intersectionX || p1.x < intersectionX)
+    if (Math.min(p0.x, p1.x) > intersectionX || Math.max(p0.x, p1.x) < intersectionX)
         return;
-    if (p2.x > intersectionX || p3.x < intersectionX)
+    if (Math.min(p2.x, p3.x) > intersectionX || Math.max(p2.x, p3.x) < intersectionX)
         return;
-    if (p0.y > intersectionY || p1.y < intersectionY)
+    if (Math.min(p0.y, p1.y) > intersectionY || Math.max(p0.y, p1.y) < intersectionY)
         return;
-    if (p2.y > intersectionY || p3.y < intersectionY)
+    if (Math.min(p2.y, p3.y) > intersectionY || Math.max(p2.y, p3.y) < intersectionY)
         return;
 
     let intersectionPt = new THREE.Vector2(intersectionX, intersectionY);
