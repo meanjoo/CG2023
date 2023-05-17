16ff2d774a518831e8b944d35c22bfad59b68b0b
meanjoo <meanjooo@naver.com>
2023-05-18 00:46:23 +0900

    edit value

--- js/compareCameraProjection.js
+++ js/compareCameraProjection.js
@@ -28,7 +28,7 @@ function initGeometry() {
     const axesHelper = new THREE.AxesHelper() // 축을 확인하는 데에 도움을 준다. x축: red, y축: green, z축: blue로 화면에 표시
     scene.add(axesHelper)
 
-    var material0 = new THREE.MeshBasicMaterial({ color: "#ff0000" })
+    var material0 = new THREE.MeshBasicMaterial({ color: "#ffffff" })
     var material1 = new THREE.MeshBasicMaterial({ color: "#00ff00"})
 
     material0.wireframe = true
@@ -39,8 +39,8 @@ function initGeometry() {
     var cube1 = new THREE.Mesh(geometryCube, material1)
 
     // Translation - x, y, z 각각의 축에 대해서 이동시킬 수 있다.
-    cube0.translateX(0.5)
-    cube0.translateZ(-0.5)
+    cube0.translateX(1.0)
+    cube0.translateZ(-1.0)
     cube1.translateZ(2.0)
 
     // Add cube to Scene
